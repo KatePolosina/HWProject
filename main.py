18c5a81b9e429b33deb34cbed45f24c3bb6b697a
Polosina <eakhokhlova@ya.ru>
2025-02-09 23:49:30 +0300

    Создана функция create_file

--- main.py
+++ main.py
@@ -2,17 +2,19 @@ import os
 import shutil
 
 
-
-def create_folder (x='new_folder'):
-    """Создание новой папки. Имя новой папки по умолчанию new_folder"""
-    folder = x
-    new_folder = folder
+def create_file(x='file'):
+    """Создание нового файла. Имя файла по умолчанию file"""
+    if not x.endswith('.txt'):
+        x += '.txt'
+    base, ext = os.path.splitext(x)
+    file = x
     i = 1
-    if os.path.exists(folder):
-        print(f'Папка {folder} существует')
-    while os.path.exists(new_folder):
-        new_folder = f'{folder}_{i}'
+    if os.path.exists(file):
+        print(f'Файл {file} существует')
+    while os.path.exists(file):
+        file = f'{base}_{i}{ext}'
         i += 1
-    os.mkdir(new_folder)
-    print(f'Создана папка {new_folder}')
+    with open(file, 'w') as f:
+        pass
+    print(f'Создан новый файл {file}')
 
