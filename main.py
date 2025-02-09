2f176db10a44231fcdb1ccf019a97a12db51421f
Polosina <eakhokhlova@ya.ru>
2025-02-09 23:54:13 +0300

    Создана функция counter

--- main.py
+++ main.py
@@ -4,11 +4,21 @@ import shutil
 
 
 
-
-def move_obj(src, dst):
-    """Перемещение файла или папки"""
-    if os.path.isfile(src):
-        print(f'Файл {src} перемещен в папку {dst}')
-    elif os.path.isdir(src):
-        print(f'Папка {src} перемещена в папку {dst}')
-    shutil.move(src, dst)
+def couter (dir=os.getcwd()):
+    """Считает количество вложенных папок и файлов"""
+    dir = dir
+    if not os.path.isdir(dir):
+        print(f'Папка {dir} отсутствует или {dir} не является папкой')
+        return
+    tree = os.walk(dir)
+    total_dirs = 0
+    total_files = 0
+    for root, dirs, files in tree:
+        dirs[:] = [x for x in dirs if not x.startswith('.') and not x.startswith('__')]
+        files = [y for y in files if not y.startswith('.')]
+        print(f'Папка: {root}')
+        total_dirs += len(dirs)
+        total_files += len(files)
+        print(f'Вложенных папок: {len(dirs)}. Вложенных файлов: {len(files)}')
+        print(f'Вложенные папки: {dirs}. Вложенные файлы: {files}')
+        print('-'*40)
