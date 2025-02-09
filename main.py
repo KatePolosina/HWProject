8a8371fe7d2af4c492c9a34dee7a638b8911ce3f
Polosina <eakhokhlova@ya.ru>
2025-02-09 23:52:38 +0300

    Создана функция copy_file

--- main.py
+++ main.py
@@ -5,12 +5,19 @@ import shutil
 
 
 
-def del_file(file):
-    """Удаление файла"""
-    if os.path.exists(file):
-        os.remove(file)
-        print(f'Файл {file} удален')
-    else:
-        print(f'Файл {file} не найден')
+
+def copy_file(src):
+    """Копирование файла"""
+    file = src
+    base, ext = os.path.splitext(src)
+    i = 1
+    if not os.path.exists(file):
+        print(f'Файл {file} отсутствует')
+        return
+    while os.path.exists(file):
+        file = f'{base}_копия_{i}{ext}'
+        i += 1
+    shutil.copy2(src, file)
+    print(f'Файл {src} скопирован в {file}')
 
 
