838f3ad35698e003577c61de0c96b865b297b754
Polosina <eakhokhlova@ya.ru>
2025-02-09 23:50:44 +0300

    Создана функция del_folder

--- main.py
+++ main.py
@@ -2,19 +2,10 @@ import os
 import shutil
 
 
-def create_file(x='file'):
-    """Создание нового файла. Имя файла по умолчанию file"""
-    if not x.endswith('.txt'):
-        x += '.txt'
-    base, ext = os.path.splitext(x)
-    file = x
-    i = 1
-    if os.path.exists(file):
-        print(f'Файл {file} существует')
-    while os.path.exists(file):
-        file = f'{base}_{i}{ext}'
-        i += 1
-    with open(file, 'w') as f:
-        pass
-    print(f'Создан новый файл {file}')
+
+def del_folder(folder):
+    """Удаление папки"""
+    shutil.rmtree(folder)
+    print(f'Папка {folder} удалена.')
+
 
