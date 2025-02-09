d8e562e737634a13cf2fc2971b62aad9f7b9ea5f
Polosina <eakhokhlova@ya.ru>
2025-02-10 01:09:53 +0300

    Создана функция find_file_txt

--- main.py
+++ main.py
@@ -1,24 +1,14 @@
 import os
-import shutil
+import re
 
+def find_file_txt(dir=os.getcwd()):
+    regex = re.compile(r'\.txt$')
+    for root, dirs, files in os.walk(dir):
+        found = True
+        for file in files:
+            if regex.search(file):
+                print(os.path.join(root, file))
+                found = True
+            if not found:
+                print(f'{root}: файлы отсутствуют')
 
-
-
-def couter (dir=os.getcwd()):
-    """Считает количество вложенных папок и файлов"""
-    dir = dir
-    if not os.path.isdir(dir):
-        print(f'Папка {dir} отсутствует или {dir} не является папкой')
-        return
-    tree = os.walk(dir)
-    total_dirs = 0
-    total_files = 0
-    for root, dirs, files in tree:
-        dirs[:] = [x for x in dirs if not x.startswith('.') and not x.startswith('__')]
-        files = [y for y in files if not y.startswith('.')]
-        print(f'Папка: {root}')
-        total_dirs += len(dirs)
-        total_files += len(files)
-        print(f'Вложенных папок: {len(dirs)}. Вложенных файлов: {len(files)}')
-        print(f'Вложенные папки: {dirs}. Вложенные файлы: {files}')
-        print('-'*40)
