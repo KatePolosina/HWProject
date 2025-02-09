e839ff70f488cd951138b09f29b77d72c9422e15
Polosina <eakhokhlova@ya.ru>
2025-02-09 23:45:56 +0300

    Создана функция create_folder

--- main.py
+++ main.py
@@ -2,7 +2,17 @@ import os
 import shutil
 
 
-def get_folder():
-    """Имя папки в которой вы находитесь"""
-    get_f = os.path.basename(os.getcwd())
-    print(f'Вы находитесь в папке: {get_f}')
+
+def create_folder (x='new_folder'):
+    """Создание новой папки. Имя новой папки по умолчанию new_folder"""
+    folder = x
+    new_folder = folder
+    i = 1
+    if os.path.exists(folder):
+        print(f'Папка {folder} существует')
+    while os.path.exists(new_folder):
+        new_folder = f'{folder}_{i}'
+        i += 1
+    os.mkdir(new_folder)
+    print(f'Создана папка {new_folder}')
+
